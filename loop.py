4cf5e80919b2aaff89933cd07557558953619015
khamsone8585 <asoulipanya@gmail.com>
2026-01-28 07:16:23 +0000

    Day 1:First miner bot Script

---
+++ loop.py
@@ -0,0 +1,5 @@
+import time
+while True:
+	print("I'm Running ++++++++")
+	time.sleep(2)
+
---
+++ miner_bot.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+
+target_ip = "8.8.8.8"
+
+response = os.system(f"ping -c 1 {target_ip} > /dev/null 2<&1")
+
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+if response == 0:
+	print(f"[{now}] IP: {target_ip} is ONLINE !!!")
+else:
+	print(f"[{now}] IP: {target_ip} is DEAD (check Now!)")
