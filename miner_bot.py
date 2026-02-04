59566b45863a7afc143f11d57357341bb317ae4b
khamsone8585 <asoulipanya@gmail.com>
2026-02-04 09:47:58 +0000

    Day 4: Completed Telegram Notification Bot v1.0

--- miner_bot.py
+++ miner_bot.py
@@ -1,27 +1,39 @@
 import os
 import datetime
+import requests 
+
+TELEGRAM_TOKEN = "8216223522:AAEvbQ_TU0I_iAIchfIQljGdK_K8FyNFezg"
+CHAT_ID = "6417593756"
+
+target_ips = ["8.8.8.8","1.1.1.1","192.168.99.99"]
+
+def send_telegram_msg(message):
+	url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+	data = {"chat_id": CHAT_ID, "text": message}
+
+	try:
+		requests.post(url, data=data)
+	except Exception as e:
+		print(f"Send Error: {e}")
+
 
 def check_ip_status(ip_address):
-	response = os.system(f"ping -c 1 {ip_address} > //dev/null 2>&1")
-	
-	if response == 0:
-		return True
-	else:
-		return False
+	response = os.system(f"ping -c 1 -W 2  {ip_address} > //dev/null 2>&1")
+	return response == 0
 
 
-target_ips = ["8.8.8.8","1.1.1.1","192.168.0.0"]
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
 print(f"------- Start Checking at {now} -----")
 
 for ip in target_ips:
-	response = os.system(f"ping -c 1 {ip} > /dev/null 2>&1")
+	is_online = check_ip_status(ip)
 
-	if response == 0:
+	if is_online:
 		print(f"[{ip}] :  is ONLINE !!!")
 	else:
-		print(f"[{ip}] : is DEAD (check Now!)")
-
+		print(f"[{ip}] : is DEAD (check Now!.....)")
+		
+		alert_msg = f" Warining \n Time: {now}\nIP: {ip} Disconnent !!!!"
+		send_telegram_msg(alert_msg)
 print("-------------------------------------------")
 
