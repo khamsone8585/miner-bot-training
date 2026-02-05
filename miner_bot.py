122b3757adea4b016067c0befbc02295a87280df
khamsone8585 <asoulipanya@gmail.com>
2026-02-05 06:26:36 +0000

    Update V1.5 : Smart Logic & state Minitoring

--- miner_bot.py
+++ miner_bot.py
@@ -1,11 +1,15 @@
 import os
 import datetime
 import requests 
+import json 
+
 
 TELEGRAM_TOKEN = "8216223522:AAEvbQ_TU0I_iAIchfIQljGdK_K8FyNFezg"
 CHAT_ID = "6417593756"
 
-target_ips = ["8.8.8.8","1.1.1.1","192.168.99.99"]
+STATE_FILE = "/root/dev_train/monitor_state.json"
+
+target_ips = ["8.8.8.8","1.1.1.1","8.8.4.4"]
 
 def send_telegram_msg(message):
 	url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
@@ -21,19 +25,39 @@ def check_ip_status(ip_address):
 	response = os.system(f"ping -c 1 -W 2  {ip_address} > //dev/null 2>&1")
 	return response == 0
 
+def load_state():
+	if os.path.exists(STATE_FILE):
+		with open(STATE_FILE, 'r') as f:
+			return json.load(f)
+	return {}
+
+def save_state(state):
+	with open(STATE_FILE, 'w') as f:
+		json.dump(state, f)
 
 now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print(f"------- Start Checking at {now} -----")
 
+last_status = load_state()
+current_status = {}
+
 for ip in target_ips:
 	is_online = check_ip_status(ip)
+	current_status[ip] = is_online
+	
+	was_online = last_status.get(ip, True)	
 
-	if is_online:
-		print(f"[{ip}] :  is ONLINE !!!")
+	if is_online and not was_online:
+		print(f"[{ip}] :  is ✅ ກັບມາໃຊ້ງານແລ້ວ !!!")
+		send_telegram_msg(f"✅ ສັນຍານກັບມາແລ້ວ!\nIP: {ip}\nເວລາ: {now}")
+	elif not is_online and was_online:
+		print(f"[{ip}] : is ຫາກະລົ້ມ !!!(ແຈ້ງເຕືອນ)")
+		send_telegram_msg(f" ແຈ້ງເຕືອນດ່ວນ!\nIP: {ip} ຂາດການເຊື່ອມຕໍ່ !\nເວລາ: {now}")
+	elif not is_online and not was_online:
+		print(f"[{ip}] : ຍັງຄົງລົ້ມຢູ່ ")
 	else:
-		print(f"[{ip}] : is DEAD (check Now!.....)")
-		
-		alert_msg = f" Warining \n Time: {now}\nIP: {ip} Disconnent !!!!"
-		send_telegram_msg(alert_msg)
+		print(f"[{ip}] : ໃຊ້ງານປົກກະຕິ ກຳລັງທຳງານ")
+
+save_state(current_status)
 print("-------------------------------------------")
 
